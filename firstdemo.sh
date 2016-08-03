32a4c5e9f04095cf7f09b4ac2660aa9a56ed7f77
Vithin Kanganda Prakash <vithin.kanganda@smartims.com>
2016-08-03 16:27:33 +0530

    this is the first file

---
+++ firstdemo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "enter a number;'
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C"
+read n
+if [][D`expr $n%2`=0 ]
+then
+echo ""[Deven number"
+else
+echo "odd number"
+fi
